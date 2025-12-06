2f3c2449faab46bf8e8d74d0430348ee33e1b888
Akash-tech102 <binduramesh0707@gmail.com>
2025-12-06 15:35:01 +0530

    Time: 0 ms (100%), Space: 57.6 MB (29.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let obj = {};
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
+        let diff = target - nums[i];
+
+        if (obj[diff] !== undefined) return [obj[diff], i];
+
+        obj[nums[i]] = i;
     }
 };

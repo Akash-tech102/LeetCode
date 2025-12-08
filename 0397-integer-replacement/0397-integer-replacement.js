d28e36ca99161cb8995097687dd157e5aa1e93c0
Akash-tech102 <binduramesh0707@gmail.com>
2025-12-08 23:25:56 +0530

    Time: 0 ms (100%), Space: 54 MB (59.49%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerReplacement = function(n) {
+    let count = 0;
+    while(n!==1){
+        if(n%2===0){
+            n= n/2;
+        }else{
+            if (n===3 || n%4===1){
+                n--;
+            }else{
+                n++;
+            }
+        }
+        count++;
+    }
+    return count;
+};

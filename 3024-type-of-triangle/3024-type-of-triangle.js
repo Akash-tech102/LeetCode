372949a9a3cabf46fe9a22c67a4857f95e56af1b
Akash-tech102 <binduramesh0707@gmail.com>
2025-12-18 15:31:49 +0530

    Time: 0 ms (100%), Space: 55.6 MB (42.05%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    let a = nums[0];
+    let b = nums[1];
+    let c = nums[2];
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return "none";
+    }
+
+    if (a === b && b === c) {
+        return "equilateral";
+    }
+    else if (a === b || b === c || a === c) {
+        return "isosceles";
+    }
+    else {
+        return "scalene";
+    }
+};
